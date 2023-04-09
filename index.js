0be62fdbffb2406280007f3ee2cd0c8e013d7d51
Brett Powers <brett.powers87@gmail.com>
2023-04-08 18:41:34 -0700

    Create the function that will play the game.

--- index.js
+++ index.js
@@ -17,3 +17,21 @@ const playerSelection = 'rock';
 const computerSeletion = getComputerChoice();
 
 
+// Make a playRound function that plays the game and takes two parameters playerSelection and computerSeletion
+function playRound(playerSelection, computerSeletion) {
+    if (playerSelection === computerSeletion)
+        return 'tie'
+
+    else if (playerSelection === 'rock' && computerSeletion === 'scissors' ||
+        playerSelection === 'scissors' && computerSeletion === 'paper' ||
+        playerSelection === 'paper' && computerSeletion === 'rock')
+        return 'Player Wins!';
+
+    else (computerSeletion === 'rock' && playerSelection === 'scissors' ||
+        computerSeletion === 'scissors' && playerSelection === 'paper' ||
+        computerSeletion === 'paper' && playerSelection === 'rock')
+    return 'Computer Wins'
+
+}
+
+console.log(playRound(playerSelection, computerSeletion));
