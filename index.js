c5fff5177bc9128b5cf15eba59b087f86e6fc8bf
Brett Powers <brett.powers87@gmail.com>
2023-04-08 18:40:38 -0700

    Create an array with rock paper scissors and a function that picks one at random.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hi')
+// Create an array of rock paper scissors
+const array = ['rock', 'paper', 'scissors'];
+
+
+// Make a function that chooses R-P-S at random
+function getComputerChoice() {
+    const random = Math.floor(Math.random() * array.length)
+    return array[random];
+}
+
+
